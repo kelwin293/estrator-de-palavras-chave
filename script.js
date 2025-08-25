1ef2f3fee8ec0b1f991f7c82f498a2c8d0a8f3f2
kelwin293 <kelwin.postanovicz@escola.pr.gov.br>
2025-08-25 08:50:32 -0300

    pablo

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMostraPalavras=document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click".mostraPalavrasChave);
+function mostraPalavrasChave(){
+    const texto=document.querySelector("#entrada-de-texto").ariaValueMax;
+    const campoResultado=document.querySelector("#resultado-palavrachave");
+    const palavraschave=processaTexto/(texto);
+    campoResultado.textContent=palavraschave.join(".");
+}
